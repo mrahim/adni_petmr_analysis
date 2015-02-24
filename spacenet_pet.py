78897179f549bf8584d884d5222ace86bb04d28b
Mehdi Rahim <rahim.mehdi@gmail.com>
2015-02-24 15:25:28 +0100

    spacenet pet

---
+++ spacenet_pet.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 24 15:17:30 2015
+
+@author: mehdi.rahim@cea.fr
+"""
+
+import os
+import numpy as np
+import nibabel as nib
+from fetch_data import fetch_adni_fdg_pet_diff, set_cache_base_dir,\
+                        set_features_base_dir, fetch_adni_masks,\
+                        set_group_indices
+from nilearn.decoding import SpaceNetClassifier
+
+CACHE_DIR = set_cache_base_dir()
+FEAT_DIR = set_features_base_dir()
+
+dataset = fetch_adni_fdg_pet_diff()
+idx = set_group_indices(dataset['dx_group'])
+pet_files = dataset['pet']
+
+
