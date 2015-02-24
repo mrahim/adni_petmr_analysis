e6005a0fa907283b1e005667e7ae43f6be1576fb
Mehdi Rahim <rahim.mehdi@gmail.com>
2015-02-24 17:47:05 +0100

    spacenet pet

--- spacenet_pet.py
+++ spacenet_pet.py
@@ -14,6 +14,24 @@ from fetch_data import fetch_adni_fdg_pet_diff, set_cache_base_dir,\
 from nilearn.decoding import SpaceNetClassifier
 from sklearn.cross_validation import StratifiedShuffleSplit
 from sklearn.metrics import accuracy_score
+from sklearn.datasets.base import Bunch
+
+####
+
+def train_and_test(X, y, spnc, train, test):
+
+    xtrain = X[train]
+    ytrain = y[train]
+    xtest = X[test]
+    ytest = y[test]    
+    spnc.fit(xtrain, ytrain)
+    ypredict = spnc.predict(xtest)
+    score = accuracy_score(ytest, ypredict)
+    coeff = spnc.coef_
+
+    return Bunch(score=score, coeff=coeff)
+
+####
 
 CACHE_DIR = set_cache_base_dir()
 FEAT_DIR = set_features_base_dir()
@@ -23,7 +41,8 @@ idx = set_group_indices(dataset['dx_group'])
 pet_files = dataset['pet']
 
 X = pet_files[idx['AD'][0]]
-X.append(pet_files[idx['Normal'][0]])
+X = np.concatenate((X, pet_files[idx['Normal'][0]]))
+
 
 y = np.ones(len(X))
 y[len(X) - len(pet_files[idx['Normal'][0]]):] = 0
@@ -32,14 +51,22 @@ y[len(X) - len(pet_files[idx['Normal'][0]]):] = 0
 mask = fetch_adni_masks()
 mask_img = nib.load(mask['mask_petmr'])
 
-
 spnc = SpaceNetClassifier(penalty='smooth-lasso', eps=1e-1,
-                          mask=mask_img, n_jobs=20, memory=CACHE_DIR)            
+                          mask=mask_img)
 
-n_iter = 1
+n_iter = 50
 sss = StratifiedShuffleSplit(y, n_iter=n_iter, test_size=.2)
 score = []
 coeff = []
+
+
+from joblib import Parallel, delayed
+p = Parallel(n_jobs=10, verbose=5)(delayed(train_and_test)\
+(X, y, spnc, train, test) for train, test in sss)
+
+np.savez_compressed('spacenet_pet_sl_50', data=p)
+
+"""
 for train, test in sss:
     xtrain = X[train]
     ytrain = y[train]
@@ -49,3 +76,4 @@ for train, test in sss:
     ypredict = spnc.predict(xtest)
     score.append(accuracy_score(ytest, ypredict))
     coeff.append(spnc.coef_)
+"""
