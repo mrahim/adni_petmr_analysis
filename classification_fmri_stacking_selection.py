30fde189a932f471c019592eadc5501fede9455f
Mehdi Rahim <rahim.mehdi@gmail.com>
2015-03-17 13:24:26 +0100

    fixed memmap issue

--- classification_fmri_stacking_selection.py
+++ classification_fmri_stacking_selection.py
@@ -8,7 +8,8 @@ Created on Mon Mar 16 11:29:51 2015
 import os, sys
 import numpy as np
 from fetch_data import set_group_indices, fetch_adni_petmr,\
-                       fetch_adni_rs_fmri_conn, set_features_base_dir
+                       fetch_adni_rs_fmri_conn, set_features_base_dir,\
+                       set_cache_base_dir
 from sklearn.cross_validation import StratifiedShuffleSplit
 from sklearn.linear_model import RidgeCV, LogisticRegression
 from sklearn.datasets.base import Bunch
@@ -43,6 +44,7 @@ def train_and_test(X, y, train, test):
     x_kbest_train_ = x_kbest_[:len(y_train), ...]
     x_kbest_test_ = x_kbest_[len(y_train):, ...]
 
+
     lgr = LogisticRegression()
     lgr.fit(x_train_, y_train)
     probas = lgr.decision_function(x_test_)
@@ -58,19 +60,31 @@ def train_and_test(X, y, train, test):
     print 'score: %.2f, k_best: %.2f' % (scores, scores_kbest)
     B = Bunch(score=scores, proba=probas, coeff=coeffs, coeff_lgr=coeff_lgr,
               score_kbest=scores_kbest, proba_kbest=probas_kbest,
-              coeff_lgr_kbest=coeff_lgr_kbest)
+              coeff_lgr_kbest=coeff_lgr_kbest,
+              xpred=np.concatenate((x_train_, x_test_)),
+              ypred=np.concatenate((y_train, y_test))
+              )
 
     return B
 
 ##############################################################################
 ##############################################################################
 FEAT_DIR = set_features_base_dir()
+CACHE_DIR = set_cache_base_dir()
+
 
 dataset = fetch_adni_rs_fmri_conn('fmri_subjects_msdl_seeds.npy')
 conn = np.load(dataset['fmri_data'], mmap_mode='r')
+
 idx = set_group_indices(dataset['dx_group'])
 
 X = conn[np.concatenate((idx['AD'], idx['EMCI'], idx['LMCI'])), ...]
+
+from joblib import dump, load
+tmp_file = os.path.join(CACHE_DIR, 'datax')
+dump(X, tmp_file)
+X = load(tmp_file, mmap_mode='r')
+
 y = np.array([1] * len(idx['AD']) + [0] * len(idx['EMCI']) + \
              [0] * len(idx['LMCI']))
 
@@ -78,11 +92,12 @@ n_iter = 100
 sss = StratifiedShuffleSplit(y, n_iter, test_size=.2,
                              random_state=np.random.seed(42))
 
+
 print 'classification'
 
 from joblib import Parallel, delayed
-
-p = Parallel(n_jobs=20, verbose=5)(delayed(train_and_test)(X, y, train, test)\
-                                   for train, test in sss)
+p = Parallel(n_jobs=20, verbose=5)\
+            (delayed(train_and_test)(X, y, train, test)\
+            for train, test in sss)
 
 np.savez_compressed(os.path.join(FEAT_DIR, 'stacking_kbest'), data=p)
